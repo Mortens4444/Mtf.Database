174cfc7df0930830c591829c17dc8de0fa90ea80
Mortens <mortens.4444@gmail.com>
2026-08-30 12:05:49 +0200

    Mtf.Database NuGet package release 6.0.34

--- Mtf.Database/Interfaces/ILoggerExtensions.cs
+++ Mtf.Database/Interfaces/ILoggerExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Mtf.Database.Extensions;
 using System;
+using System.Globalization;
 using System.Runtime.InteropServices.JavaScript;
+using System.Text.RegularExpressions;
 
 namespace Mtf.Database.Interfaces;
 
@@ -9,7 +11,7 @@ public static partial class ILoggerExtensions
 {
     public static void Log(this ILogger logger, Exception exception, string message, params object[] args)
     {
-        var msg = String.Format(message, args);
+        var msg = FormatMessageTemplate(message, args);
         var exceptionDetails = String.Concat(msg, " - ", exception.ToFullExceptionString());
 
         LogError(logger, exceptionDetails);
@@ -35,4 +37,13 @@ public static partial class ILoggerExtensions
 
     [LoggerMessage(Level = LogLevel.Error, Message = "An error occurred: {Message}")]
     private static partial void LogError(ILogger logger, string message);
+
+    private static readonly Regex PlaceholderRegex = new("{[^{}]+}", RegexOptions.Compiled);
+
+    private static string FormatMessageTemplate(string message, object[] args)
+    {
+        var index = 0;
+        return PlaceholderRegex.Replace(message, match =>
+            index < args.Length ? Convert.ToString(args[index++], CultureInfo.InvariantCulture) ?? string.Empty : match.Value);
+    }
 }
